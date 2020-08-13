dad5bb723ff7a004706efa8b13776276f0e1a713
= <darkewizzard@gmail.com>
2020-08-13 13:22:47 -0400

    move admin code into seperate JS file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
 const { v4: uuidv4 } = require('uuid');
+const admin = require('./source/admin');
 
 //Create Express App
 const app = express();
@@ -37,7 +38,7 @@ const pool = new Pool({
 
 //App Default Repsonse
 app.get('/', (req, res) => {
-    res.render('pages/home',{query:req.query});
+  res.render('pages/home',{query:req.query});
 });
 
 //DATABASE Response
@@ -48,38 +49,7 @@ app.get(['/data','/data/*'], async (req,res)=>{
 
 //ADMIN ONLY TEST
 app.get('/admin', async (req,res)=>{
-  let cookie = req.cookies["session"];
-  if(cookie == null){
-    res.render('pages/auth',{page:'admin'});
-  }else{
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM sessions');
-      const data = { 'results': (result) ? result.rows : null};
-      var success = false;
-      var now = new Date();
-      for(var row in data.results){
-        var date = new Date(data.results[row].expiration);
-        if(date<now){
-          let query = 'DELETE FROM sessions WHERE name = \''+data.results[row].name+ '\'';
-          await client.query(query);
-        }else if(data.results[row].name == cookie){
-          success = true;
-        }
-      }
-      
-      if(success){
-        res.render('pages/admin');
-      }else{
-        res.render('pages/auth',{page:'admin'});
-      }
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  }
-  
+  admin.load(req,res,pool,'admin');
 });
 
 //App DB Response
@@ -98,45 +68,11 @@ app.get('/db', async (req,res)=>{
 
 //App FORM POST
 app.post('/auth', async function(req, res){
-  if(req.body.password == process.env.AUTH_PW){
-    let session_name = ""+uuidv4();
-    var session_exp = new Date();
-    session_exp.setDate(session_exp.getDate() + 1);
-    let query = 'INSERT INTO sessions(name,expiration) VALUES ( \''+session_name+'\',\''+session_exp.toISOString()+'\');';
-
-    try {
-      const client = await pool.connect();
-      await client.query(query);
-      client.release();
-    } catch (err) {
-      console.error(err);
-    }
-
-
-    res.cookie('session',session_name, { maxAge: 900000, httpOnly: true, sameSite: "strict", secure: true });
-    let page = 'pages/'+req.body.page;
-    res.render(page,{query:req.query});
-  }else{
-    res.render('pages/home',{query:req.query});
-  }
+  admin.login(req,res,pool,process.env.AUTH_PW);
 });
 
 app.post('/logout', async function(req, res){
-  let cookie = req.cookies["session"];
-  cookie = cookie.replace(/[^a-zA-Z0-9-_]+/ig,'');
-  if(cookie != null){
-    let query = 'DELETE FROM sessions WHERE name = \''+cookie+ '\'';
-    try {
-      const client = await pool.connect();
-      await client.query(query);
-      client.release();
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  res.clearCookie("session");
-  res.render('pages/home',{query:req.query});
+  admin.logout(req,res,pool);
 });
 
 //Start App on given port
